c6256bb250362a1fc5d06b8234d997d0aeb8fe7d
Dade1991 <dade91@msn.com>
2025-07-10 17:02:46 +0200

    Added some comments

--- Exercise/pexel-start.js
+++ Exercise/pexel-start.js
@@ -1,10 +1,13 @@
+// mi creo la costante x il link API
+
 const apiLink = `https://api.pexels.com/v1/search?query=hamsters`
+
+//  in questo caso, dovrmo utilizzare una "autorizazzione" per poter avere il via libera ad accedere all'API link tramite: authorization.
 
 const getPicture = function () {
   fetch(apiLink, {
     headers: {
       Authorization: `nwywgKyXbmELfyhuRqPVXyeIbmokqQsa29KiypHCV9UB1ftlb7ITKNPV`,
-      // Authorization: `l4Rf3ZW27ACB7DOlwbOWnK3xNBVFRAD60hiVtMSdFEkxp1hCd9yH1tRg`,
     },
   })
     .then(function (response) {
@@ -14,11 +17,18 @@ const getPicture = function () {
         throw new Error(`Error-: ${response.statusText}`)
       }
     })
+    // richiamo all'array generico dell'API dove sono contenute le immagini che devo caricare ex-nuovo.
     .then(function (arrayOfInfo) {
+      // richiamo gli elementi HTML su cui dovrò intervenire.
       const oldImg = document.querySelectorAll(`.card-img-top`)
+      // estrapolo dall'API l'array specifico dove sono contenute le info che mi servono
       const newImg = arrayOfInfo.photos
+      console.log(arrayOfInfo)
 
+      // ciclo le immagini presenti sull'HTML dando istruzioni su cosa fare e due parametri (img > accedo al sottolivello di arrayOfInfo & i, l'indicizzazione di ciascun elemento), ovvero, cambiarle con quelle nuove (newImg[i].src.original serve x accedere ancora più in profondità e recuperare esattamente quello che mi serve, ovvero una IMG proveniente da API Link in formato/dimensione "originale")
       oldImg.forEach(function (img, i) {
+        console.log(i)
+
         if (newImg[i]) {
           img.src = newImg[i].src.original
         }
