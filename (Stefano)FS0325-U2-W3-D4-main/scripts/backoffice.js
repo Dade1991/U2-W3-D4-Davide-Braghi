15c03999be439fd0d64b0595ccfac05b076d91ca
Dade1991 <dade91@msn.com>
2025-07-25 17:46:58 +0200

    Other Changes

--- (Stefano)FS0325-U2-W3-D4-main/scripts/backoffice.js
+++ (Stefano)FS0325-U2-W3-D4-main/scripts/backoffice.js
@@ -1,28 +1,31 @@
-const endpoint = 'https://striveschool-api.herokuapp.com/api/agenda'
+const endpoint = "https://striveschool-api.herokuapp.com/api/agenda"
 // -PARTE FINALE-
 // ora la pagina backoffice servirà anche per MODIFICARE un evento già
 // esistente. In tal caso, si caricherà con un parametro "eventId" nell'URL.
 const parameters = new URLSearchParams(location.search) // creo un oggetto con TUTTI i parametri in questo URL
-const eventId = parameters.get('eventId')
+const eventId = parameters.get("eventId")
+
+console.log(eventId)
+
 // ora eventId potrebbe essere una stringa oppure null, a seconda che la pagina
 // sia stata caricata in modalità "modifica" (a partire da un bottone modifica)
 // oppure in modalità "creazione" (l'utente ha cliccato il link nella navbar)
 if (eventId) {
   // ora dobbiamo fare la fetch e ripopolare la pagina
-  fetch(endpoint + '/' + eventId)
+  fetch(endpoint + "/" + eventId)
     .then((response) => {
       if (response.ok) {
         return response.json()
       } else {
-        throw new Error('errore nel recupero dati del singolo concerto')
+        throw new Error("errore nel recupero dati del singolo concerto")
       }
     })
     .then((concertDetails) => {
       console.log(concertDetails)
-      document.getElementById('name').value = concertDetails.name
-      document.getElementById('description').value = concertDetails.description
-      document.getElementById('price').value = concertDetails.price
-      document.getElementById('time').value = concertDetails.time.slice(0, -5)
+      document.getElementById("name").value = concertDetails.name
+      document.getElementById("description").value = concertDetails.description
+      document.getElementById("price").value = concertDetails.price
+      document.getElementById("time").value = concertDetails.time.slice(0, -5)
     })
     .catch((err) => {
       console.log(err)
@@ -40,7 +43,7 @@ class Concert {
 
 const yearInFooter = function () {
   // per prima cosa, popolo il footer con l'anno corrente
-  const span = document.getElementById('year')
+  const span = document.getElementById("year")
   span.innerText = new Date().getFullYear() // 2025
 }
 
@@ -55,14 +58,14 @@ const yearInFooter = function () {
 
 // interagiamo ora con l'evento di submit del form in modo
 // da poter inviare gli oggetti che creiamo all'API dei concerti
-const concertForm = document.getElementById('concert-form')
-concertForm.addEventListener('submit', (e) => {
+const concertForm = document.getElementById("concert-form")
+concertForm.addEventListener("submit", (e) => {
   e.preventDefault()
   //   recupero i riferimenti ai 4 input
-  const nameInput = document.getElementById('name')
-  const descriptionInput = document.getElementById('description')
-  const priceInput = document.getElementById('price')
-  const timeInput = document.getElementById('time')
+  const nameInput = document.getElementById("name")
+  const descriptionInput = document.getElementById("description")
+  const priceInput = document.getElementById("price")
+  const timeInput = document.getElementById("time")
 
   const concertToSave = new Concert(
     nameInput.value, // valore dell'input name
@@ -80,7 +83,7 @@ concertForm.addEventListener('submit', (e) => {
   let endpointToUse
   if (eventId) {
     // abbiamo un ID nell'url, siamo qua per MODIFICARE un concerto!
-    endpointToUse = endpoint + '/' + eventId
+    endpointToUse = endpoint + "/" + eventId
   } else {
     // NON abbiamo un ID nell'url., siamo qua per CREARE un concerto!
     endpointToUse = endpoint
@@ -89,17 +92,17 @@ concertForm.addEventListener('submit', (e) => {
   let methodToUse
   if (eventId) {
     // abbiamo un ID nell'url, siamo qua per MODIFICARE un concerto!
-    methodToUse = 'PUT'
+    methodToUse = "PUT"
   } else {
     // NON abbiamo un ID nell'url., siamo qua per CREARE un concerto!
-    methodToUse = 'POST'
+    methodToUse = "POST"
   }
 
   fetch(endpointToUse, {
     method: methodToUse, // il metodo POST indica che questa chiamata genererà una nuova risorsa
     body: JSON.stringify(concertToSave), // il body dev'essere una stringa JSON
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
       // sto dicendo alle API che allego un oggetto in formato JSON
     },
   })
@@ -107,16 +110,16 @@ concertForm.addEventListener('submit', (e) => {
       if (response.ok) {
         // il concerto dovrebbe essersi salvato correttamente!
         // l'operazione è andata a buon fine!
-        alert('OPERAZIONE COMPLETATA!')
+        alert("OPERAZIONE COMPLETATA!")
         // svuotiamo il form
         concertForm.reset()
       } else {
-        throw new Error('Errore nella request', response.status)
+        throw new Error("Errore nella request", response.status)
       }
     })
     .catch((err) => {
-      alert('ERRORE')
-      console.log('ERRORE', err)
+      alert("ERRORE")
+      console.log("ERRORE", err)
     })
 })
 
